b310f25bb6df1a1604dff711aa36b4507639b143
Dante Stivaktas <stivaktasd@gmail.com>
2026-01-29 07:48:08 -0600

    Modify SQL script to show user privileges
    Updated SQL script to show grants for specific users.

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
--- Task: List all users
--- Select all users from mysql.user
-SELECT user, host FROM mysql.user;
+-- Lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server (in localhost).
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
