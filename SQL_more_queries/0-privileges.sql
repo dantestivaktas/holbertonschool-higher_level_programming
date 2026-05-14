cb4efa053b4309b092eba5fc8d0f05e0b37b4426
Dante Stivaktas <stivaktasd@gmail.com>
2026-05-14 00:37:00 -0600

    Update 0-privileges.sql

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,4 +1,2 @@
--- Lists all privileges of the MySQL users user_0d_1 and user_0d_2 on the server (in localhost).
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
--- Lists all privileges of the MySQL users user_0d_2 on the server (in localhost).
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
